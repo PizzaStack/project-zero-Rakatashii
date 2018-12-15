6e6badcb0ed99bb740cace99c1d0d3c44e191876
christianmeyer <cmeyerwps@gmail.com>
2018-12-14 19:52:16 -0600

    # WARNING: head commit changed in the meantime
    "

--- BankApp/src/main/java/com/BankApp/App.java
+++ BankApp/src/main/java/com/BankApp/App.java
@@ -9,7 +9,7 @@ public class App
 	
     public static void main( String[] args )
     {
-    	System.out.println("check in git");
+    	//System.out.println("check in git");
         System.out.println( "Hello World!" );
     }
 }
