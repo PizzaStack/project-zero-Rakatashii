0f070580d118e03cbf9424e8f762bde2ebc4acd9
christianmeyer <cmeyerwps@gmail.com>
2018-12-14 19:54:08 -0600

    test pizza_party and rakatashii repositories

--- BankApp/src/main/java/com/BankApp/App.java
+++ BankApp/src/main/java/com/BankApp/App.java
@@ -10,6 +10,7 @@ public class App
     public static void main( String[] args )
     {
     	//System.out.println("check in git");
+    	System.out.println("check in git2");
         System.out.println( "Hello World!" );
     }
 }
